29a240edcf56fca21dd7df03589cac4c5aa43b47
Naseef03 <muhammednaseef03@gmail.com>
2024-06-26 20:48:47 +0530

    Adds laod_data function to load data from files as matrix

--- handler.py
+++ handler.py
@@ -2,4 +2,11 @@ def save_row(row, file_path="data.csv"):
     with open(file_path, 'a') as file:
         file.write(", ".join([str(i) for i in row]) + "\n")
     print("Row:", row, "saved to", file_path)
-    
+
+
+def load_data(file_path="data.csv"):
+    data = []
+    with open(file_path, "r") as file:
+        for row in file.readlines():
+            data.append([int(i) for i in row.strip("\n").split(", ")])
+    return data
