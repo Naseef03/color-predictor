e30d45ce0a219d62934649b3e8d90d39f5493fa9
Naseef03 <muhammednaseef03@gmail.com>
2024-07-03 22:55:30 +0530

    Adds checker for save_row function

--- handler.py
+++ handler.py
@@ -1,4 +1,9 @@
 def save_row(row, file_path="data.csv"):
+    # Check the length of row
+    if len(row) != 11:
+        print("Length of data should be 11")
+        return
+    
     with open(file_path, 'a') as file:
         file.write(", ".join([str(i) for i in row]) + "\n")
     print("Row:", row, "saved to", file_path)
